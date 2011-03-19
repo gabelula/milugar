bbbca5ce18abccf93c4b6e975743f19d05962376
Gabriela Rodriguez <gabelula@gmail.com>
2011-03-19 14:16:40 -0300

    first commit with the new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milugar::Application.config.secret_token = '4435ea612be603725395ffb0c1165449407c6cd485329d423776bc1e74c1469bc60a2c07d5314d7b63fd25471a20a9b03be2bdda379d17fea944e7adb200ecd2'
